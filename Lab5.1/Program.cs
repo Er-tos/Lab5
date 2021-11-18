c2821abdd20cf256dce79fe923aea6a9ca732c3d
Анатолий Ершов <a.v.ershov@sgugit.ru>
2021-11-18 22:19:53 +0700

    Добавьте файлы проекта.

---
+++ Lab5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 7 элементов массива:");
+            int[] array = new int[7];
+            float S = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+            }
+            Console.WriteLine("Среднее арифметическое элементов: {0:f2}", S / 7);
+            Console.ReadKey();
+        }
+    }
+}
